c6524a721c511250ee3e114559f1439a75986665
Thiago Luan <112329988+thiagoluann@users.noreply.github.com>
2022-09-06 12:18:20 -0300

    Add files via upload

---
+++ Questões 1, 2 e 3.c
@@ -0,0 +1,15 @@
+// 1) X = 3 Y = 4 P = 4
+
+// 2)
+
+// a) mensagem de erro
+
+// b) diz que é uma conversão inválida
+
+// c) a execução não foi bem sucedida
+
+// d) na linna 6 estava "p=x", com isso estava errado, no entanto modifiquei para "p=&x"
+
+// e) com essa alteração a execução foi bem sucedida
+
+// 3) 30 20 10
